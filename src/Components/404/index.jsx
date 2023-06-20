4f705f81e33847021bbb05d87ee2485bb216f9ff
Francisco Reyes Callado <reypakin@gmail.com>
2023-06-20 15:18:41 -0600

    Actualizar estilos CSS -  BEM, index.jsx 404

--- src/Components/404/index.jsx
+++ src/Components/404/index.jsx
@@ -1,13 +1,17 @@
 import "./404.css";
 import image from "../../../public/img/404.gif";
+import { Link } from "react-router-dom";
 
 function Error404() {
   return (
-    <main className="Error404">
+    <main className="main__404">
       <img className="image-404" src={image} alt="404" />
       <p className="notfound-text">
-        Esta página no existe o esta en mantenimiento
+        Esta página no existe o está en mantenimiento :c
       </p>
+      <Link className="notfound-text__link" to="/">
+        Regresa al Inicio
+      </Link>
     </main>
   );
 }
