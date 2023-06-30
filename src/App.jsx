7a5042019c855d57dcc33ac10ebb636ec538a89c
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2023-06-29 18:23:43 -0600

    Añado cards de videos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Home from "./Components/Home";
+import { Youtube } from "./Components/Home/Videos";
 
 function App() {
+  // esta es la función para leer la lista de videos de youtube para la pagina de inicio
+
+  const video = Youtube.map((Video, index) => {
+    return <Home lista={Video} key={index} />;
+  });
   return (
     <>
-      <Home />
+      <section>{video}</section>
     </>
   );
 }
--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,14 +1,25 @@
-import Card from "./Card";
-import { Youtube } from "./Videos";
+import React from "react";
+import ReactPlayer from "react-player";
+import "./video.css";
 
-function Home() {
-  const video = Youtube.map((Video, index) => {
-    return <Card lista={Video} key={index} />;
-  });
+function Home(props) {
+  const { titulo, autor, video } = props.lista;
   return (
-    <>
-      <section>{video}</section>
-    </>
+    <div className=" m-2 d-inline-flex flex-wrap">
+      <div className="card rounded" style={{ width: "400px" }}>
+        <ReactPlayer
+          className="video"
+          width="100%"
+          height="350px"
+          url={video}
+          controls
+        />
+        <div className="card-body">
+          <h2 className="card-text fs-3">{titulo}</h2>
+          <p className="card-text">Autor: {autor}</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
