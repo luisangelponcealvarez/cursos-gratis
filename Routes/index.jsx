89265eddd8a40f63a632f09110dfeb188aaa8007
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2023-06-29 23:08:00 -0600

    añado sitios y modificaciónes

--- Routes/index.jsx
+++ Routes/index.jsx
@@ -1,14 +1,14 @@
-// import App from "../src/App";
+import App from "../src/App";
 import Error404 from "../src/Components/404";
 
 import { createBrowserRouter } from "react-router-dom";
 import Luisangelponce from "../src/site/Luisangelponce";
 
 const routes = [
-  // {
-  //   path: "/",
-  //   element: <App />,
-  // },
+  {
+    path: "/",
+    element: <App />,
+  },
   {
     path: "/Luis_Angel_Ponce_Alvarez",
     element: <Luisangelponce />,
