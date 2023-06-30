12c91e68da0d92478c7da31b37c2012c329c72c9
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2023-06-29 20:42:54 -0600

    añadon mensaje

--- Routes/index.jsx
+++ Routes/index.jsx
@@ -1,13 +1,13 @@
-import App from "../src/App";
+// import App from "../src/App";
 import Error404 from "../src/Components/404";
 
 import { createBrowserRouter } from "react-router-dom";
 
 const routes = [
-  {
-    path: "/",
-    element: <App />,
-  },
+  // {
+  //   path: "/",
+  //   element: <App />,
+  // },
   {
     path: "/*",
     element: <Error404 />,
