bfe09bbf83022f2ac6d42d427bbd130ced2e30fc
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2023-07-09 20:26:34 -0600

    Añado mensaje de esta pagina esta en mantenimiento

--- Routes/index.jsx
+++ Routes/index.jsx
@@ -2,22 +2,17 @@ import App from "../src/App";
 import Error404 from "../src/Components/404";
 
 import { createBrowserRouter } from "react-router-dom";
-import Luisangelponce from "../src/site/Luisangelponce";
 import GiaFrontEnd from "../src/site/gia";
 
 const routes = [
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/Luis_Angel_Ponce_Alvarez",
-    element: <Luisangelponce />,
-  },
-  {
-    path: "/GiaFrontEnd",
-    element: <GiaFrontEnd/>
-  },
+  // {
+  //   path: "/",
+  //   element: <App />,
+  // },
+  // {
+  //   path: "/GiaFrontEnd",
+  //   element: <GiaFrontEnd/>
+  // },
   {
     path: "/*",
     element: <Error404 />,
