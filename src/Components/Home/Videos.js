34e5eddf3237b435ec49ae7d248b257e4505de95
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2023-07-09 20:08:37 -0600

    añado mas videos y mi sitio

--- src/Components/Home/Videos.js
+++ src/Components/Home/Videos.js
@@ -33,4 +33,39 @@ export const Youtube = [
     autor: "TodoCode",
     video: "https://www.youtube.com/watch?v=YsdypbvNrgw",
   },
+  {
+    titulo: "Por este motivo no voy a la universidad",
+    autor: "Soy Dalto",
+    video: "https://youtu.be/ATpPsxzjF-k",
+  },
+  {
+    titulo: "JavaScript para Manipulación del DOM - Curso con Proyectos",
+    autor: "freeCodeCamp",
+    video: "https://youtu.be/koiPxFFiqJ4",
+  },
+  {
+    titulo: "Aprende Python ahora! curso completo e intensivo desde cero",
+    autor: "HolaMundo",
+    video: "https://youtu.be/tQZy0U8s9LY",
+  },
+  {
+    titulo: "Curso de GIT y GITHUB desde CERO para PRINCIPIANTES",
+    autor: "MoureDev by Brais Moure",
+    video: "https://youtu.be/3GymExBkKjE",
+  },
+  {
+    titulo: "De Novato a Ninja: Aprende TypeScript - curso intensivo(#1)",
+    autor: "midulive",
+    video: "https://youtu.be/fUgxxhI_bvc",
+  },
+  {
+    titulo: "Aprende Node.js y Express - Curso desde Cero",
+    autor: "freeCodeCamp",
+    video: "https://www.youtube.com/watch?v=1hpc70_OoAg&t=16s",
+  },
+  {
+    titulo: "Aprende JavaScript en 10 minutos",
+    autor: "Programador X",
+    video: "https://www.youtube.com/watch?v=8GTaO9XhA5M&t=602s",
+  },
 ];
