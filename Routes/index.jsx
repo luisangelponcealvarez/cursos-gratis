eb8127bd12f57a4e1d2080765d780b67d8ed4a4d
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2023-07-11 21:30:09 -0600

    vista previa de la versión del sitio del home

--- Routes/index.jsx
+++ Routes/index.jsx
@@ -2,17 +2,17 @@ import App from "../src/App";
 import Error404 from "../src/Components/404";
 
 import { createBrowserRouter } from "react-router-dom";
-// import GiaFrontEnd from "../src/site/gia";
+import GiaFrontEnd from "../src/site/gia";
 
 const routes = [
-  // {
-  //   path: "/",
-  //   element: <App />,
-  // },
-  // {
-  //   path: "/GiaFrontEnd",
-  //   element: <GiaFrontEnd />,
-  // },
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/GiaFrontEnd",
+    element: <GiaFrontEnd />,
+  },
   {
     path: "/*",
     element: <Error404 />,
