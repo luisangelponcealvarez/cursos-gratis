2a3e0856830596fa81aab96dad4324afd20a40cd
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2023-07-11 22:15:58 -0600

    trato de acomodar la parte del celular del home

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -5,17 +5,8 @@ function Home(props) {
   const { titulo, autor, video } = props.lista;
   return (
     <div className="Home d-inline-flex flex-wrap">
-      <div
-        className="card"
-        style={{ border: "2px solid black", borderRadius: "15px" }}
-      >
-        <ReactPlayer
-          className="video"
-          width="100%"
-          height="270px"
-          url={video}
-          controls
-        />
+      <div className="card">
+        <ReactPlayer className="video" url={video} controls />
         <div className="card-body">
           <h1 className="card-text fs-5">{titulo}</h1>
           <p className="card-text">Autor(@): {autor}</p>
