b2261ce75b5d538c86ea904a378ec77edf498e8f
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2023-09-05 19:15:19 -0600

    añado una sección mas de la gia front end

--- src/site/Gias/GiaFrontEnd/index.jsx
+++ src/site/Gias/GiaFrontEnd/index.jsx
@@ -163,6 +163,38 @@ export function GiaFrontEnd() {
             </section>
           </div>
         </div>
+
+        <div className="row my-5">
+          <div className="col">
+            <hr />
+            <h3>4. Frameworks de Front-end</h3>
+            <hr />
+
+            <section>
+              <h3>React:</h3>
+              <ReactPlayer
+                url="https://www.youtube.com/watch?v=7iobxzd_2wY&pp=ygUFUmVhY3Q%3D"
+                controls={true} // Muestra los controles de reproducción (pausa, reproducción, volumen, etc.).
+              />
+            </section>
+
+            <section>
+              <h3>Angular:</h3>
+              <ReactPlayer
+                url="https://www.youtube.com/watch?v=sS90VVmBPcg&pp=ygUNY3Vyc28gQW5ndWxhcg%3D%3D"
+                controls={true} // Muestra los controles de reproducción (pausa, reproducción, volumen, etc.).
+              />
+            </section>
+
+            <section>
+              <h3>Vue.js:</h3>
+              <ReactPlayer
+                url="https://www.youtube.com/watch?v=7yc5RuAVHqo&pp=ygUQY3Vyc28gZGUgVnVlLmpzOg%3D%3D"
+                controls={true}
+              />
+            </section>
+          </div>
+        </div>
       </div>
       <Footer />
     </>
