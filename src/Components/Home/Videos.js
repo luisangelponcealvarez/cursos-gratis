44650ec98f350a996bc409b97657df8d804264c8
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2023-09-09 12:01:50 -0600

    añado estilos del home

--- src/Components/Home/Videos.js
+++ src/Components/Home/Videos.js
@@ -1,38 +1,31 @@
 export const Youtube = [
   {
     titulo: "Donde buscar ideas para crear tu sitio web",
-    autor: "Irina Delgado",
     video: "https://www.youtube.com/watch?v=YiGZHoHE6rY",
   },
 
   {
     titulo: "Tutorial Completo de Markdown",
-    autor: "FalconMasters",
     video: "https://www.youtube.com/watch?v=_hI14xuvQag",
   },
   {
     titulo: "Por este motivo no voy a la universidad",
-    autor: "Soy Dalto",
     video: "https://youtu.be/ATpPsxzjF-k",
   },
   {
     titulo: "Curso de GIT y GITHUB desde CERO para PRINCIPIANTES",
-    autor: "MoureDev by Brais Moure",
     video: "https://youtu.be/3GymExBkKjE",
   },
   {
     titulo: "De Novato a Ninja: Aprende TypeScript - curso intensivo(#1)",
-    autor: "midulive",
     video: "https://youtu.be/fUgxxhI_bvc",
   },
   {
     titulo: "Aprende Node.js y Express - Curso desde Cero",
-    autor: "freeCodeCamp",
     video: "https://www.youtube.com/watch?v=1hpc70_OoAg&t=16s",
   },
   {
     titulo: "Aprende JavaScript en 10 minutos",
-    autor: "Programador X",
     video: "https://www.youtube.com/watch?v=8GTaO9XhA5M&t=602s",
   },
 ];
